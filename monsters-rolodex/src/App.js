a01ab440687625888be8929ac5c9b550d1fe3c26
elise tang <elisetang23@gmail.com>
2020-05-22 18:06:40 -0400

    understant key/state/map

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,8 +8,21 @@ constructor(){
   super();
 
   this.state = {
-    string:'Hello World'
-  }
+    monster:[
+      {
+        name: 'Frankenstein',
+        id:'asc1'
+      },
+      {
+        name:'Dracula',
+        id:'asc2'
+      },
+      {
+        name:'Zombie',
+        id:'asc3'
+      }
+    ]
+  };
 }
 //ended constructor
 
@@ -18,13 +31,9 @@ constructor(){
   render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         {this.state.string}
-        </p>
-        <button onClick={()=>this.setState({string:'Hello React'})} >Change Text</button>
-      </header>
+      {
+        this.state.monster.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+      }
     </div>
   );    
   }
